bef69ea887ddeb4d49797524a388d7b89c1e1b86
ShangMing Cherng <night1300013@gmail.com>
2017-10-26 23:03:47 -0700

    checkpoint 13 completed

---
+++ scripts/player-bar.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+  $('#play-pause').click(function(){
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+
+  $('button#next').click(function(){
+    if (player.playState !== 'playing') { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+
+    if (album.songs[currentSongIndex + 1]){
+      const nextSong = album.songs[currentSongIndex + 1];
+      player.playPause(nextSong);
+    }
+  });
+
+});
