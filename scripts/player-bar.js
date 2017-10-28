fb8d26d5e5b7ee3ee3385e7208b509f43cf5253c
ShangMing Cherng <night1300013@gmail.com>
2017-10-27 23:49:51 -0700

    assignment-13-buttons completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,4 +14,13 @@ $( document ).ready(function() {
     }
   });
 
+  $('button#previous').click(function(){
+    if (player.playState !== 'playing'){ return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+
+    if (album.songs[currentSongIndex - 1]) {
+        const previousSong = album.songs[currentSongIndex - 1];
+        player.playPause(previousSong);
+    }
+  });
 });
